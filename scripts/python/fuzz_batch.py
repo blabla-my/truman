402feb826f39acea4648c56b55831e37959d7bff
MingyuanLuo <myluo24@m.fudan.edu.cn>
2026-04-30 20:10:42 +0800

    Add fuzz batch output option

--- scripts/python/fuzz_batch.py
+++ scripts/python/fuzz_batch.py
@@ -1,16 +1,19 @@
 import subprocess
 import argparse
+import shlex
+from pathlib import Path
 from multiprocessing import Pool
 from fuzz import QEMUFuzz
 
 
-def run_fuzz_on_target(seq, target, round, asan = False, fork = False):
+def run_fuzz_on_target(output, seq, target, round, asan = False, fork = False):
     # python3 ~/truman/scripts/python/fuzz.py -e --fork -t virtio-scsi --tool truman
     fork_arg = "--fork" if fork else ""
+    output_arg = f"-o {shlex.quote(str(output))}" if output else ""
     if asan:
-        cmd = f"sleep {seq} && python3 ~/truman/scripts/python/fuzz.py --asan {fork_arg} -t {target} --tool truman"
+        cmd = f"sleep {seq} && python3 ~/truman/scripts/python/fuzz.py --asan {fork_arg} -t {target} --tool truman {output_arg}"
     else:
-        cmd = f"sleep {seq} && python3 ~/truman/scripts/python/fuzz.py -e {fork_arg} -t {target} --tool truman"
+        cmd = f"sleep {seq} && python3 ~/truman/scripts/python/fuzz.py -e {fork_arg} -t {target} --tool truman {output_arg}"
     print(f"[+] fuzz {target}, round {round}")
     print(f"[CMD] {cmd}")
     p = subprocess.run(cmd, shell=True, text=True)
@@ -20,7 +23,7 @@ def main(args):
     tasks = []
     for seq,target in enumerate(targets):
         for i in range(args.repeat):
-            tasks.append((seq, target, i, args.asan))
+            tasks.append((args.output, seq, target, i, args.asan, args.fork))
     
     with Pool(processes = min(len(tasks), args.jobs)) as pool:
         pool.starmap(run_fuzz_on_target, tasks)
@@ -31,5 +34,7 @@ if __name__ == "__main__":
     parser.add_argument('--repeat', type=int, default=1)
     parser.add_argument('--asan', action='store_true', default=False)
     parser.add_argument('--fork', action='store_true', default=False)
+    parser.add_argument('-o', '--output', type=Path, default=None,
+                        help='Output root directory passed to fuzz.py')
     args = parser.parse_args()
     main(args)
